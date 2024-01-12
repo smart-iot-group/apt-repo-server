13c263d705ce9ba86cfe310949c78bd93867ba74
trisjakobismart <130749762+trisjakobismart@users.noreply.github.com>
2024-01-12 20:48:41 +0100

    Terminate running supervisord socket thing

--- startup.sh
+++ startup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Function to handle termination signals
+_term() {
+  echo "Caught signal, stopping supervisord..."
+  /usr/bin/supervisorctl stop all
+  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf shutdown
+  exit 0
+}
+
+# Catch termination signals
+trap _term SIGTERM SIGINT
+
 mkdir -p /data/dists/trusty/main/binary-amd64/
-exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+# Start supervisord
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+# Wait for supervisord to exit
+wait $!
